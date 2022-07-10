e61d5b418e3f9612c3b0779c23da02e2f23a9e24
Alan Benavides <bruno@coastt.org>
2022-07-09 20:38:48 -0400

    add: about menu

--- app/src/main/java/com/example/quiz/fragment/Home.kt
+++ app/src/main/java/com/example/quiz/fragment/Home.kt
@@ -1,12 +1,12 @@
 package com.example.quiz.fragment
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.quiz.R
 
 
@@ -20,7 +20,23 @@ class Home : Fragment() {
         buttonA?.setOnClickListener { view: View ->
             Navigation.findNavController(view).navigate(R.id.action_home_to_game)
         }
+        setHasOptionsMenu(true)
 
         return view.rootView
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.game_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.about -> {
+                NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
